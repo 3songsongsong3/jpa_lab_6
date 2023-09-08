d35345e52872619db50003c5c03a070a9a5cfaf4
tlahsh12 <tlahsh12@naver.com>
2023-09-08 20:20:03 +0900

    Initial commit

---
+++ src/main/java/entity/Member.java
@@ -0,0 +1,15 @@
+package entity;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@AttributeOverrides({
+        @AttributeOverride(name = "id", column = @Column(name = "MEMBER_ID")),
+        @AttributeOverride(name = "name", column = @Column(name = "MEMBER_NAME"))
+})
+public class Member extends BaseEntity{
+
+}
